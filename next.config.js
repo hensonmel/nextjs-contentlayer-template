b96ee30484eefbc834c792c70ca99dde2bac9a80
hensonmel <hnsnml@hnsnml.co>
2024-11-26 13:33:17 +0800

    chore: remove build warnings

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const { withContentlayer } = require("next-contentlayer2")
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = withContentlayer(nextConfig)
